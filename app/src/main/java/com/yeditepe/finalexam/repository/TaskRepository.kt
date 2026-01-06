eef5a1047b060c58e4cad02f48fb41fd94914159
ArdaBlr0 <arda.bulur@std.yeditepe.edu.tr>
2026-01-06 15:21:52 +0300

    update1

--- app/src/main/java/com/yeditepe/finalexam/repository/TaskRepository.kt
+++ app/src/main/java/com/yeditepe/finalexam/repository/TaskRepository.kt
@@ -7,7 +7,9 @@ class TaskRepository(private val api: TaskApi) {
 
     suspend fun fetchTasks(): List<Task> {
         // TODO 1: Call API
+        val taskDto = api.getTasks()
         // TODO 2: Convert TaskDto list to Task list
+
         return TODO("Provide the return value")
     }
 }
